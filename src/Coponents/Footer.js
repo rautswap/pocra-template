45c0ff2b3de3d51ed3fcbbf60adf252aaec2fa51
rautswapnil007 <Swapnil.Raut@mahait.org>
2021-08-10 11:30:37 -0700

    Update link in footer

--- src/Coponents/Footer.js
+++ src/Coponents/Footer.js
@@ -5,9 +5,10 @@ export default class Footer extends Component {
         return (
             <div>
                 <footer className="main-footer">
-                    <strong>Copyright © 2021 <a href="https://adminlte.io">PoCRA</a>.</strong> All rights reserved.
+                    <strong>Copyright © 2021 <a href="http://gis.mahapocra.gov.in/controlpanel/">PoCRA</a>.</strong> All rights reserved.
                     <div className="float-right d-none d-sm-inline-block">
-                        <b>Version</b> 1.0.1
+                        <b>Version</b> 1.0.0
+
                     </div>
                 </footer>
 
