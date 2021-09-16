837beda794550c020cfb4fbd40786d3a28147af2
rautswapnil007 <swapnil.raut@mahait.org>
2021-09-16 14:54:02 +0530

    update in dbt legend

--- src/Components/DBTComponent/LegendPanelDashboard.js
+++ src/Components/DBTComponent/LegendPanelDashboard.js
@@ -8,53 +8,9 @@ export default class LegendPanelDashboard extends Component {
 	}
 
 	render() {
-
-
-		const renderAuthButton = () => {
-
-
-			return (
-				<>
-					<table >
-						{
-							console.log(this.props)
-						}
-						{/* <tr>
-							<th colSpan={2} className="borber" > Legend</th>
-						</tr>
-						<tr>
-							<th colSpan={2}> No of Applications</th>
-						</tr>
-						<tr>
-							<td><img src="dist/legend/forcast1.jpg" height={'30px'} width={'50px'} />  </td>
-
-							<td>0 - {this.props.props.appl_1} </td>
-						</tr>
-						<tr>
-							<td><img src="dist/legend/forcast2.jpg" height={'30px'} width={'50px'} />  </td>
-
-							<td>{this.props.props[0].appl_1 + 0.1} - {this.props.props[0].appl_2} </td>
-						</tr>
-						<tr>
-							<td><img src="dist/legend/forcast3.jpg" height={'30px'} width={'50px'} />  </td>
-
-							<td>{this.props.props[0].appl_2 + 0.1} - {this.props.props[0].appl_3} </td>
-						</tr>
-						<tr>
-							<td><img src="dist/legend/forcast4.jpg" height={'30px'} width={'50px'} />  </td>
-
-							<td>{this.props.props[0].appl_3 + 0.1} and above </td>
-						</tr> */}
-					</table>
-				</>
-			)
-
-
-		}
+	
 		return (
 			<div>
-				{/* {renderAuthButton()} */}
-				return (
 				<>
 					<table >
 						{
@@ -67,33 +23,33 @@ export default class LegendPanelDashboard extends Component {
 							<th colSpan={2}> No of Applications</th>
 						</tr>
 						<tr>
-							<td><img src="dist/legend/forcast1.jpg" height={'30px'} width={'50px'} />  </td>
+							<td><img src="dist/legend/appl_1.png" height={'30px'} width={'50px'} />  </td>
 
 							<td>0 - {this.props.props.appl_1} </td>
 						</tr>
 						<tr>
-							<td><img src="dist/legend/forcast2.jpg" height={'30px'} width={'50px'} />  </td>
+							<td><img src="dist/legend/appl_2.png" height={'30px'} width={'50px'} />  </td>
 
 							<td>{this.props.props.appl_1 + 0.1} - {this.props.props.appl_2} </td>
 						</tr>
 						<tr>
-							<td><img src="dist/legend/forcast3.jpg" height={'30px'} width={'50px'} />  </td>
+							<td><img src="dist/legend/appl_3.png" height={'30px'} width={'50px'} />  </td>
 
 							<td>{this.props.props.appl_2 + 0.1} - {this.props.props.appl_3} </td>
 						</tr>
 						<tr>
-							<td><img src="dist/legend/forcast3.jpg" height={'30px'} width={'50px'} />  </td>
+							<td><img src="dist/legend/appl_4.png" height={'30px'} width={'50px'} />  </td>
 
 							<td>{this.props.props.appl_3 + 0.1} - {this.props.props.appl_4} </td>
 						</tr>
 						<tr>
-							<td><img src="dist/legend/forcast3.jpg" height={'30px'} width={'50px'} />  </td>
+							<td><img src="dist/legend/appl_5.png" height={'30px'} width={'50px'} />  </td>
 
 							<td>{this.props.props.appl_4 + 0.1} - {this.props.props.appl_5} </td>
 						</tr>
 					</table>
 				</>
-				)
+
 			</div>
 		)
 
